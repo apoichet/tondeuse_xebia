350aab7df0fd7a30439080575568e3bf94a91804
apoichet <apoichet@gmail.com>
2016-03-11 14:58:06 +0100

    Firts commit to import project

---
+++ Tondeuse/src/com/xebia/tondeuse/lanceur/Lanceur.java
@@ -0,0 +1,54 @@
+package com.xebia.tondeuse.lanceur;
+
+import java.util.Map;
+
+import com.xebia.tondeuse.commun.OrientationEnum;
+import com.xebia.tondeuse.commun.TondeuseUtils;
+import com.xebia.tondeuse.object.Surface;
+import com.xebia.tondeuse.object.Tondeuse;
+import com.xebia.tondeuse.service.ActionTondeuseService;
+import com.xebia.tondeuse.service.LireInstructionService;
+import com.xebia.tondeuse.service.impl.ActionTondeuseServiceImpl;
+import com.xebia.tondeuse.service.impl.LireInstructionServiceImpl;
+
+
+public class Lanceur{
+
+	public static void main(String[] args) {
+		
+		//Les service
+		LireInstructionService insctructionService = new LireInstructionServiceImpl();
+		ActionTondeuseService tondeuseService = new ActionTondeuseServiceImpl();
+
+		//Les instructions
+		Map<String, String> mapInstructions = insctructionService.lireFichierInstruction(TondeuseUtils.NOM_FICHIER);
+		int tailleSurface[] = insctructionService.getTailleSurface(mapInstructions.get(TondeuseUtils.KEY_SURFACE));
+		String position1[] = insctructionService.getPositionOrientationTondeuse(mapInstructions.get(TondeuseUtils.KEY_COORDONNEE_1));
+		String position2[] = insctructionService.getPositionOrientationTondeuse(mapInstructions.get(TondeuseUtils.KEY_COORDONNEE_2));
+		String instruction1 = insctructionService.getInstruction(mapInstructions.get(TondeuseUtils.INSTRUCTION_1));
+		String instruction2 = insctructionService.getInstruction(mapInstructions.get(TondeuseUtils.INSTRUCTION_2));
+		
+		//La surface
+		Surface surface = new Surface(tailleSurface[0], tailleSurface[1]);
+		
+		//Les tondeuses
+		int positionX1 = TondeuseUtils.formatingCaractere(position1[0]);
+		int positionY1 = TondeuseUtils.formatingCaractere(position1[1]);
+		OrientationEnum orient1 = OrientationEnum.getOrientationEnum(position1[2]);
+		Tondeuse tondeuse1 = tondeuseService.positionnerTondeuse(1, positionX1, positionY1, orient1, surface);
+		
+		int positionX2 = TondeuseUtils.formatingCaractere(position2[0]);
+		int positionY2 = TondeuseUtils.formatingCaractere(position2[1]);
+		OrientationEnum orient2 = OrientationEnum.getOrientationEnum(position2[2]);
+		Tondeuse tondeuse2 = tondeuseService.positionnerTondeuse(2, positionX2, positionY2, orient2, surface);
+		
+		//On fait bouger la tondeuse numéro 1
+		tondeuseService.bougerTondeuse(tondeuse1, tondeuse2, instruction1);
+		System.out.println(tondeuse1.toString());
+		//On fait bouger la tondeuse numéro 2
+		tondeuseService.bougerTondeuse(tondeuse2, tondeuse1, instruction2);
+		System.out.println(tondeuse2.toString());
+		
+	}
+
+}
---
+++ Tondeuse/src/com/xebia/tondeuse/service/impl/ActionTondeuseServiceImpl.java
@@ -0,0 +1,191 @@
+package com.xebia.tondeuse.service.impl;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.xebia.tondeuse.commun.OrientationEnum;
+import com.xebia.tondeuse.commun.TondeuseUtils;
+import com.xebia.tondeuse.object.Surface;
+import com.xebia.tondeuse.object.Tondeuse;
+import com.xebia.tondeuse.service.ActionTondeuseService;
+
+public class ActionTondeuseServiceImpl implements ActionTondeuseService {
+
+	
+	
+	
+	@Override
+	public void bougerTondeuse(Tondeuse tondeuseMoove, Tondeuse tondeuseStatic,
+			String instructions) {
+
+		if (StringUtils.isNotEmpty(instructions)) {
+
+			char tabInsctruct[] = instructions.toCharArray();
+
+			for (char instruct : tabInsctruct) {
+				
+				traiterInstruct(tondeuseMoove, tondeuseStatic, instruct);
+
+			}
+
+		}
+
+	}
+
+	@Override
+	public Tondeuse positionnerTondeuse(int numeroTondeuse, int positionX,
+			int positionY, OrientationEnum orientation, Surface surface) {
+
+		Tondeuse tondeuse = new Tondeuse(numeroTondeuse, positionX, positionY,
+				orientation, surface);
+
+		// On evite de faire déborder la tondeuse de la surface
+		boolean depasseLargeur = depasseLargeur(positionX, surface);
+		if (depasseLargeur) {
+			tondeuse.setPositionX(surface.getLargeur());
+		}
+		
+		boolean depasseLongeur = depasseLongueur(positionY, surface);
+		if (depasseLongeur) {
+			tondeuse.setPositionY(surface.getLongueur());
+		}
+
+		return tondeuse;
+
+	}
+
+	protected void traiterInstruct(Tondeuse tondeuseMoove,
+			Tondeuse tondeuseStatic, char instruction) {
+
+		int coordonnee[] = new int[2];
+		coordonnee[0] = tondeuseMoove.getPositionX();
+		coordonnee[1] = tondeuseMoove.getPositionY();
+		OrientationEnum orientation = tondeuseMoove.getOrientationEnum();
+
+		switch (instruction) {
+		case TondeuseUtils.CODE_AVANCER:
+
+			coordonnee = avancerTondeuse(tondeuseMoove, tondeuseStatic);
+			break;
+
+		case TondeuseUtils.CODE_DROITE:
+
+			orientation = OrientationEnum.getOrientationEnumRight(orientation);
+			break;
+
+		case TondeuseUtils.CODE_GAUCHE:
+
+			orientation = OrientationEnum.getOrientationEnumLeft(orientation);
+			break;
+		}
+
+		tondeuseMoove.setOrientationEnum(orientation);
+		tondeuseMoove.setPositionX(coordonnee[0]);
+		tondeuseMoove.setPositionY(coordonnee[1]);
+
+	}
+
+	protected int[] avancerTondeuse(Tondeuse tondeuseMoove, Tondeuse tondeuseStatic) {
+
+		OrientationEnum orientation = tondeuseMoove.getOrientationEnum();
+		int positionX = tondeuseMoove.getPositionX();
+		int positionY = tondeuseMoove.getPositionY();
+		
+		int positionFinalX = tondeuseMoove.getPositionX();
+		int positionFinalY = tondeuseMoove.getPositionY();
+
+		if (OrientationEnum.NORD.equals(orientation)) {
+			positionY++;
+
+		} else if (OrientationEnum.SUD.equals(orientation)) {
+			positionY--;
+
+		} else if (OrientationEnum.WEST.equals(orientation)) {
+			positionX--;
+			
+		} else if (OrientationEnum.EST.equals(orientation)) {
+			positionX++;
+
+		}
+		
+		if (isOkDeplacement(positionX, positionY, tondeuseMoove, tondeuseStatic)) {
+			positionFinalX = positionX;
+			positionFinalY = positionY;
+		}
+		
+		int coordonne[] = {positionFinalX, positionFinalY};
+		
+		return coordonne;
+
+	}
+	
+	protected boolean isOkDeplacement(int positionX, int positionY, Tondeuse tondeuseMoove, Tondeuse tondeuseStatic){
+		
+		boolean okFrontiere = isOkFrontiereSurface(positionX, positionY, tondeuseMoove);
+		
+		boolean pasColision = !isColision(positionX, positionY, tondeuseStatic);
+		
+		return okFrontiere && pasColision;
+		
+	}
+
+	protected boolean isOkFrontiereSurface(int positionX, int positionY, Tondeuse tondeuseMoove) {
+
+		Surface surface = tondeuseMoove.getSurface();
+		
+		boolean ok = !(depasseLargeur(positionX, surface) || depasseLongueur(positionY, surface));
+
+		return ok;
+	}
+	
+	protected boolean isColision(int positionX, int positionY, Tondeuse tondeuseStatic){
+		boolean colision = false;
+		
+		if (tondeuseStatic != null) {
+			
+			if (positionX == tondeuseStatic.getPositionX() && positionY == tondeuseStatic.getPositionY()) {
+			
+				System.out.println(TondeuseUtils.MSG_COLISION);
+				colision = true;
+			}
+			
+		}
+		
+		return colision;
+		
+	}
+	
+	protected boolean depasseLargeur(int positionX, Surface surface){
+		boolean depasse = false;
+		
+		if (surface != null) {
+			
+			int largeur = surface.getLargeur();
+			
+			if (largeur < positionX) {
+				
+				depasse = true;
+				
+			}
+			
+		}
+		return depasse;
+	}
+	
+	protected boolean depasseLongueur(int positionY, Surface surface){
+		boolean depasse = false;
+		
+		if (surface != null) {
+			
+			int longueur = surface.getLongueur();
+			
+			if (longueur < positionY) {
+				
+				depasse = true;
+				
+			}
+			
+		}
+		return depasse;
+	}
+
+}
